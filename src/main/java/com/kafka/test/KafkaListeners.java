6a7f5f33cd50a31bc6477eaf1b23dd1b1fd2cb1e
Bhuvan A <bhuvan.a@happiestminds.com>
2023-02-18 15:50:50 +0530

    first commit

---
+++ src/main/java/com/kafka/test/KafkaListeners.java
@@ -0,0 +1,16 @@
+package com.kafka.test;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+
+	@KafkaListener(
+			topics= "test",
+			groupId= "groupId"
+	)
+	void listener(String data) {
+		System.out.println("Listner received: "+ data);
+	}
+}
